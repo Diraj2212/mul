998aeca109c64346d57951aba9526b91604c9008
Chintu <neelamdheerajkumar1122@gmail.com>
2024-08-25 14:11:37 +0530

    hi from mul

---
+++ mul.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number: 20"
+
+# Ask user for second number
+echo "Enter second number: 10"
+
+# Perform multiplication
+prod=$((20 * 10))
+
+# Display result
+echo "Product: $prod"
+
+
